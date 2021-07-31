a88c251888825925b89bbc1fdc1d4bc678d6e8f6
Yeonuk Hwang <younuk23@gmail.com>
2021-08-01 05:57:26 +0900

    add reset

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
+import "./src/reset.css"
 // custom CSS styles
 import "./src/style.css"
 
