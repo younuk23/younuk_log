855c22142eb70aca320fc9d2fa946130ff10349f
Yeonuk Hwang <younuk23@gmail.com>
2021-08-04 01:24:58 +0900

    Add: Github-Typography-add - Github Typography add - delete normalizecss and style.css

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import GitHubTheme from "typography-theme-github"
+
+const typography = new Typography(GitHubTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
